fd8ebfe6038433f39601750da18b201845b82689
David Tram <dattram0610@gmail.com>
2018-07-04 15:21:03 -0700

    Added fields and functionality for Adding New Containers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/dash-board/navigation-bar/navigation-bar.component';
@@ -17,6 +19,8 @@ import { InvoiceFormComponent } from './components/dash-board/invoice-form/invoi
   imports: [
     BrowserModule,
     AlertModule.forRoot(),
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/dash-board/invoice-form/invoice-form.component.ts
+++ src/app/components/dash-board/invoice-form/invoice-form.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Invoice } from '../../../models/invoice';
+
 @Component({
   selector: 'app-invoice-form',
   templateUrl: './invoice-form.component.html',
   styleUrls: ['./invoice-form.component.css']
 })
 export class InvoiceFormComponent implements OnInit {
+  invoice: Invoice;
+  isAddingContainer: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.invoice = new Invoice();
+    this.isAddingContainer = false;
   }
 
+  addNewContainer() {
+    this.isAddingContainer = true;
+  }
+
+  saveNewContainer() {
+    this.isAddingContainer = false;
+  }
+
+  submitInvoice() {
+    console.log(this.invoice);
+  }
 }
