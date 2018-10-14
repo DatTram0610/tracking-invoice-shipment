1b341e7dbc3b79d4aa52b32f29159697e93f5492
David Tram <dattram0610@gmail.com>
2018-10-14 15:47:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/tracking-invoice-shipment'));
 app.get('*', (request, response) => {
 	response.sendFile(path.join(__dirname, 'dist/tracking-invoice-shipment', 'index.html'));
 });
