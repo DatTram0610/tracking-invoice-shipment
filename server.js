ae2636f6932a4c84476302b897db5325ef20ecb4
David Tram <dattram0610@gmail.com>
2018-10-14 15:29:04 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'dist/tracking-invoice-shipment', 'index.html'));
+});
 app.listen(process.env.PORT || 8080);
